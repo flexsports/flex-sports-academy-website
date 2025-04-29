04f7bf5ec28d9ee090331d4070fd5d67d978e2fb
JaeMin Im <ljm930324@gmail.com>
2025-04-30 00:17:21 +0900

    thrid commit

--- components/common/KakaoMap.tsx
+++ components/common/KakaoMap.tsx
@@ -7,48 +7,21 @@ interface KakaoMapProps {
   address: string;
 }
 
-interface KakaoLatLng {
-  getLat(): number;
-  getLng(): number;
-}
-
-interface KakaoMap {
-  setCenter(position: KakaoLatLng): void;
-}
-
-interface KakaoMarker {
-  setMap(map: KakaoMap | null): void;
-}
-
 interface GeocodeResult {
   x: string;
   y: string;
 }
 
-interface KakaoMapsService {
-  Geocoder: new () => {
-    addressSearch: (
-      address: string,
-      callback: (result: GeocodeResult[], status: string) => void
-    ) => void;
-  };
-  Status: {
-    OK: string;
-  };
-}
-
-interface KakaoMaps {
-  load: (callback: () => void) => void;
-  LatLng: new (lat: number, lng: number) => KakaoLatLng;
-  Map: new (container: HTMLElement, options: { center: KakaoLatLng; level: number }) => KakaoMap;
-  Marker: new (options: { position: KakaoLatLng }) => KakaoMarker;
-  services: KakaoMapsService;
-}
-
 declare global {
-  interface Window {
-    kakao: {
-      maps: KakaoMaps;
+  namespace kakao.maps.services {
+    class Geocoder {
+      addressSearch(
+        address: string,
+        callback: (result: GeocodeResult[], status: string) => void
+      ): void;
+    }
+    const Status: {
+      OK: string;
     };
   }
 }
